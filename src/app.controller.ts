f335ba4d195634f67dd122be27017e78d5cb0a78
Uhmpasterig <805luca805@gmail.com>
2024-03-22 16:23:02 +0100

    feat: sync

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('USER_SERVICE') private client: ClientProxy,
+  ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    this.client.send<number>({ cmd: 'getHello' }, {});
+    return 'done.';
   }
 }
